cbf49e513d9be64f25c17333cc1fb9e35cc45d9d
AllPro3d <flopana77@gmail.com>
2023-04-16 10:34:55 +0700

    logging enabled for debugging

--- klippy/extras/firmware_retraction_zhop.py
+++ klippy/extras/firmware_retraction_zhop.py
@@ -1,8 +1,10 @@
 # Support for Marlin/Smoothie/Reprap style firmware retraction via G10/G11
 #
 # Copyright (C) 2019  Len Trigg <lenbok@gmail.com>
+# Copyright (C) 2023  Florian-Patrice Nagel <flopana77@gmail.com>
 #
 # This file may be distributed under the terms of the GNU GPLv3 license.
+import logging
 
 class FirmwareRetraction:
     def __init__(self, config):
@@ -132,33 +134,43 @@ class FirmwareRetraction:
         # Unregister the original G1 method from the G1 command and
         # store the associated method in prev_cmd
         prev_cmd = self.gcode.register_command("G1", None)
+        logging.debug("unregister_G1: Previous G1 command registred")
 
         # Now, register the original G1 method with the new G1.20140114 command,
         # and set its description to indicate it's a renamed built-in command
         pdesc = "Renamed builtin of '%s'" % ("G1")
         self.gcode.register_command('G1.1', prev_cmd, desc=pdesc)
+        logging.debug("unregister_G1: Original G1 method re-registred to G1.20140114")
+        
         # Register the G0 and the G1 commands with the z-hop G1 method
         self.gcode.register_command('G0', self.cmd_G1_zhop)
+        logging.debug("unregister_G1: New G1 method registred to G0")
+        
         cmd_desc = "G1 command that accounts for z hop when retracted"
         self.gcode.register_command('G1', self.cmd_G1_zhop, desc=cmd_desc)
+        logging.debug("unregister_G1: New G1 method registred to G1")
     
     ##########################################################################################  Re-registrer old G1 command handler
     def re_register_G1(self):
         # Unregister the original G1 method from the G1.20140114 command and
         # store the associated method in prev_cmd
         prev_cmd = self.gcode.register_command("G1.1", None)
+        logging.debug("re_register_G1: Previous G1 command registred")
 
         # Now, register the original G1 method with the old G1 command,
         # and set empty description
         self.gcode.register_command("G1", prev_cmd, desc=None)
+        logging.debug("re_register_G1: Register original G1 method to G1 command")
         
         # Re-register the G0 command with the original G1 method
         self.gcode.register_command('G0', self.gcode_move.cmd_G1)
+        logging.debug("re_register_G1: Register original G1 method to G0 command")
 
     
     ######################################################################################### G1 method that accounts for z-hop by altering the z-coordinates
     def cmd_G1_zhop(self,gcmd):
         params = gcmd.get_command_parameters()
+        logging.debug("cmd_G1_zhop: Params received")
         
         # Check if there's a Z movement in the command
         if 'Z' in params:
