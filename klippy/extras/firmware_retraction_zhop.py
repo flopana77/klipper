972842a3c2ee1a08e3dc64174bf0613ef0119138
AllPro3d <flopana77@gmail.com>
2023-05-11 23:21:08 +0700

    added z_hop_style to  set_retraction command

--- klippy/extras/firmware_retraction_zhop.py
+++ klippy/extras/firmware_retraction_zhop.py
@@ -17,6 +17,9 @@ class FirmwareRetraction:
         # Get a reference to the gcode_move object
         self.gcode_move = printer.lookup_object('gcode_move')
         
+        # Define valid z_hop styles
+        self.valid_z_hop_styles = ['standard','ramp', 'helix']
+        
         # Initialize various retraction-related parameters from the config
         self.retract_length = config.getfloat('retract_length', 0., minval=0.)
         self.retract_speed = config.getfloat('retract_speed', 20., minval=1)
@@ -29,12 +32,7 @@ class FirmwareRetraction:
         
         ############################################################################################################### Added z_hop_style to config, "Linear" or "Helix" for Bambu Lab style zhop. format all lower case and define valid inputs.
         self.z_hop_style = config.get('z_hop_style', default='standard').strip().lower()
-        self.valid_z_hop_styles = ['standard','ramp', 'helix']
-        
-        ############################################################################################################### Check that z_hop_style was input and is valid.
-        if self.z_hop_style not in self.valid_z_hop_styles:
-            self.z_hop_style = 'standard'
-            logging.warning('The provided z_hop_style value is invalid. Using "standard" as default.')
+        self._check_z_hop_style()
         
         # Initialize unretract length, retracted state and ramp move flag
         self.unretract_length = (self.retract_length + self.unretract_extra_length)
@@ -85,6 +83,7 @@ class FirmwareRetraction:
             
             ################################################################################################################ Added back z_hop_height with 2mm minimum CHANGE LATER
             self.z_hop_height = gcmd.get_float('Z_HOP_HEIGHT', self.z_hop_height, minval=0.)
+            self.z_hop_style = gcmd.get('Z_HOP_STYLE', self.z_hop_style).strip().lower()
             
             self.unretract_length = (self.retract_length + self.unretract_extra_length)
             self.is_retracted = False
@@ -261,7 +260,13 @@ class FirmwareRetraction:
 
         # Run the G1.20140114 command with the adjusted parameters
         self.gcode.run_script_from_command(new_g1_command)
-    
+        
+    def _check_z_hop_style(self):   
+        ############################################################################################################### Check that z_hop_style was input and is valid.
+        if self.z_hop_style not in self.valid_z_hop_styles:
+            self.z_hop_style = 'standard'
+            logging.warning('The provided z_hop_style value is invalid. Using "standard" as default.')
+            
 # Function to load the FirmwareRetraction class from the configuration file
 def load_config(config):
     return FirmwareRetraction(config)
